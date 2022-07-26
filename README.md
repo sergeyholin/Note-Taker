083c399d88220d816937b347b964cb673d19398c
Sergey Holin <holin_sergey@yahoo.com>
2022-07-25 20:04:25 -0700

    App is functional & demo ready

---
+++ README.md
@@ -0,0 +1,44 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+# Note-Taker
+Module 11 Challenge by Sergey Holin
+
+GitHub Repo Link: https://github.com/sergeyholin/Note-Taker
+
+Heroku Live Deployment Link: 
+
+## ABOUT THE APP:
+
+This application is a simple note taker program which uses Express.js for back end framework and saves & retrieves data from a JSON file.
+
+## ACCEPTANCE CRITERIA:
+
+```
+GIVEN a note-taking application
+WHEN I open the Note Taker
+THEN I am presented with a landing page with a link to a notes page
+WHEN I click on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page
+WHEN I click on the Save icon
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
+WHEN I click on the Write icon in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+```
+
+## TECHNOLOGIES USED: 
+
+NODE.js (https://nodejs.org/en/)
+
+Express.js (https://expressjs.com/)
+
+## CITATIONS:
+
+In server.js line 57 was inspired by an answer on stack overflow by GregL.
+Source: (https://stackoverflow.com/questions/65015000/how-do-i-use-express-js-app-delete-to-remove-a-specific-object-from-an-array#:~:text=Assuming%20your%20array%20is%20just,req%2C%20res)%20%7B%20console.)
+
+## SCREENSHOT:
+
+<img src="./assets/img/screenshot.png/"/>
--- server.js
+++ server.js
@@ -2,61 +2,65 @@ const express = require ('express');
 const fs = require ('fs');
 const util = require ('util');
 const uuid = require("./helpers/uuid")
-
 const path = require('path');
 const { readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils');
-
-const PORT = 3001;
+// Added process.env.port for Heroku deployment
+const PORT = process.env.PORT || 3001;
 const app = express();
-
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-// -------------------------------------------------------------
-// Get route for homepage
+// GET route for index HTML homepage.
 app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-// Get route for notes page
+// GET route for notes HTML page.
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-// ----------------------------------
-// Get route for retrieving all notes
+// GET route to retrieve all notes.
 app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received`);
+    console.info(`${req.method} request received.`);
+    // Using Read From File helper function to display all notes from the database.
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
-// POST route for new note
+// POST route to make a new note.
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received`);
-  
-    const { title, text } = req.body;
-  
+    console.info(`${req.method} request received.`); 
+    const { title, text } = req.body; 
     if (req.body) {
       const newNote = {
         title,
         text,
         id: uuid(),
-
       };
-  
+      // Using Read & Append helper function to display and add new note to the database.
       readAndAppend(newNote, './db/db.json');
-      res.json(`Note added successfully 🚀`);
+      res.json(`Note added successfully!`);
     } else {
-      res.error('Error in adding note');
+      res.error('Error in adding note.');
     }
   });
-  // Delete route
-  app.delete(`/api/notes/:id`, (req, res) => {
-    console.info(`${req.method} request received`);
-    // res.send("DELETE Request Called")
-      readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-      console.log(data)
+// BONUS: Delete request!
+// DELETE ROUTE function for deleting notes.
+app.delete(`/api/notes/:id`, (req, res) => {
+  // Make a DELETE request, log a message.
+  console.info(`${req.method} request received.`);
+  // Console log and make a variable for note object id property.
+  // console.log("req params", req.params.id)
+  const { id } = req.params.id;
+  // READ entire note object data and turn it into JSON format.
+  readFromFile('./db/db.json').then((notesData) => {
+      let data = JSON.parse(notesData);
+      // This function filters through all the notes and collects everything except the note that has an ID assosiated with delete request.
+      data = data.filter(({ id }) => id !== req.params.id);
+      // Now we take all the notes, except the one we just excluded, and we write them all back into our dabase.
+      writeToFile('./db/db.json', data);
+      res.json(`Note deleted successfully!`);
+    });
   });
-  
-//   -----------------------------
+// This function is used to bind and listen the connections on the specified host and port.
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
